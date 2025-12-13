9ca7b93047d58d642a99274eb4b9435e64d6fcd8
SeverinGusenleitner <severin.gusenleitner@gmail.com>
2025-12-13 15:45:36 +0100

    added update function, now the extended class is useful, because the cat and dog have different update functions

--- projects/2025-12-12-cat-and-dog/src/animal.ts
+++ projects/2025-12-12-cat-and-dog/src/animal.ts
@@ -27,12 +27,36 @@ draw():void{
     this.animal.style.top = `${this.y}px`
     this.animal.style.left = `${this.x}px`
 }
-
+abstract update():void;
 }
 
 export class Dog extends Animal{
-
+    private vx:number 
+    constructor(x:number,y:number,image:string,vx:number){
+        super(x,y,image)
+        this.vx = vx;
+    }
+    update(){
+        const width = this.outerDiv?.clientWidth;
+        this.x += this.vx;
+        if(this.x>width!||this.x<0){
+            this.vx = -this.vx;
+        }
+    }
 }
-export class Cat extends Animal{
 
+export class Cat extends Animal{
+    private vy:number;
+    constructor(x:number,y:number,image:string,vy:number){
+        super(x,y,image);
+        this.vy = vy;
+    }
+
+    update(){
+        const height = this.outerDiv?.clientHeight;
+        this.y += this.vy;
+        if(this.y>height!||this.y<0){
+            this.vy = -this.vy;
+        }
+    }
 }
--- projects/2025-12-12-cat-and-dog/src/sim.ts
+++ projects/2025-12-12-cat-and-dog/src/sim.ts
@@ -5,21 +5,22 @@ import dogimg from '../assets/dog.png';
 import catimg from '../assets/cat.png';
 export class Simulation{
     private animals:Animal[] = [];
-    public createCat(){
-        const x = Math.random()*400;
-        const y = Math.random()*400;
-        const cat = new Dog(x,y,catimg);
-        this.animals.push(cat);
-    }
     public createDog(){
         const x = Math.random()*400;
         const y = Math.random()*400;
-        const dog = new Cat(x,y,dogimg);
+        const cat = new Dog(x,y,dogimg,1);
+        this.animals.push(cat);
+    }
+    public createCat(){
+        const x = Math.random()*400;
+        const y = Math.random()*400;
+        const dog = new Cat(x,y,catimg,1);
         this.animals.push(dog);
     }
     public animate(){
         for(const animal of this.animals){
             animal.draw();
+            animal.update();
         }
         requestAnimationFrame(()=>this.animate()!);
     }
