2211ba2037e14c8748b5847ecbc61fa3f287f23a
SeverinGusenleitner <severin.gusenleitner@gmail.com>
2025-12-13 15:48:10 +0100

    made the cat and dog not go past bounds, just changed the if statement a bit so it activates at canvaswidth-animalwidth

--- projects/2025-12-12-cat-and-dog/src/animal.ts
+++ projects/2025-12-12-cat-and-dog/src/animal.ts
@@ -1,62 +1,58 @@
-export  abstract class Animal{
-protected readonly outerDiv = document.getElementById("canvas");
-protected animal:HTMLImageElement;
-protected x:number;
-protected y:number;
-protected readonly width:number = 100;
-protected readonly height:number = 100;
-constructor(x:number,y:number,image:string){
- this.x = x;
- this.y = y;
+export abstract class Animal {
+  protected readonly outerDiv = document.getElementById('canvas');
+  protected animal: HTMLImageElement;
+  protected x: number;
+  protected y: number;
+  protected readonly width: number = 100;
+  protected readonly height: number = 100;
+  constructor(x: number, y: number, image: string) {
+    this.x = x;
+    this.y = y;
 
- this.animal = document.createElement("img")
- this.animal.src = image;
- this.animal.style.width = `${this.width}px`;
- this.animal.style.height = `${this.height}px`;
- this.animal.classList.add("animal");
- this.animal.style.top = `${this.y}`
- this.animal.style.left = `${this.x}`
-
-
-    this.animal.style.position = "absolute"
-
-
+    this.animal = document.createElement('img');
+    this.animal.src = image;
+    this.animal.style.width = `${this.width}px`;
+    this.animal.style.height = `${this.height}px`;
+    this.animal.classList.add('animal');
+    this.animal.style.top = `${this.y}`;
+    this.animal.style.left = `${this.x}`;
+    this.animal.style.position = 'absolute';
     this.outerDiv!.appendChild(this.animal);
-}
-draw():void{
-    this.animal.style.top = `${this.y}px`
-    this.animal.style.left = `${this.x}px`
-}
-abstract update():void;
-}
-
-export class Dog extends Animal{
-    private vx:number 
-    constructor(x:number,y:number,image:string,vx:number){
-        super(x,y,image)
-        this.vx = vx;
-    }
-    update(){
-        const width = this.outerDiv?.clientWidth;
-        this.x += this.vx;
-        if(this.x>width!||this.x<0){
-            this.vx = -this.vx;
-        }
-    }
+  }
+  draw(): void {
+    this.animal.style.top = `${this.y}px`;
+    this.animal.style.left = `${this.x}px`;
+  }
+  abstract update(): void;
 }
 
-export class Cat extends Animal{
-    private vy:number;
-    constructor(x:number,y:number,image:string,vy:number){
-        super(x,y,image);
-        this.vy = vy;
+export class Dog extends Animal {
+  private vx: number;
+  constructor(x: number, y: number, image: string, vx: number) {
+    super(x, y, image);
+    this.vx = vx;
+  }
+  update() {
+    const width = this.outerDiv?.clientWidth;
+    this.x += this.vx;
+    if (this.x > width!-this.width || this.x < 0) {
+      this.vx = -this.vx;
     }
+  }
+}
 
-    update(){
-        const height = this.outerDiv?.clientHeight;
-        this.y += this.vy;
-        if(this.y>height!||this.y<0){
-            this.vy = -this.vy;
-        }
+export class Cat extends Animal {
+  private vy: number;
+  constructor(x: number, y: number, image: string, vy: number) {
+    super(x, y, image);
+    this.vy = vy;
+  }
+
+  update() {
+    const height = this.outerDiv?.clientHeight;
+    this.y += this.vy;
+    if (this.y > height! -this.width|| this.y < 0) {
+      this.vy = -this.vy;
     }
+  }
 }
--- projects/2025-12-12-cat-and-dog/src/sim.ts
+++ projects/2025-12-12-cat-and-dog/src/sim.ts
@@ -1,27 +1,30 @@
-import { Animal } from "./animal";
-import {Cat} from "./animal";
-import { Dog } from "./animal";
+import { Animal } from './animal';
+import { Cat } from './animal';
+import { Dog } from './animal';
+
 import dogimg from '../assets/dog.png';
 import catimg from '../assets/cat.png';
-export class Simulation{
-    private animals:Animal[] = [];
-    public createDog(){
-        const x = Math.random()*400;
-        const y = Math.random()*400;
-        const cat = new Dog(x,y,dogimg,1);
-        this.animals.push(cat);
+
+
+export class Simulation {
+  private animals: Animal[] = [];
+  public createDog() {
+    const x = Math.random() * 400;
+    const y = Math.random() * 400;
+    const cat = new Dog(x, y, dogimg, 1);
+    this.animals.push(cat);
+  }
+  public createCat() {
+    const x = Math.random() * 400;
+    const y = Math.random() * 400;
+    const dog = new Cat(x, y, catimg, 1);
+    this.animals.push(dog);
+  }
+  public animate() {
+    for (const animal of this.animals) {
+      animal.draw();
+      animal.update();
     }
-    public createCat(){
-        const x = Math.random()*400;
-        const y = Math.random()*400;
-        const dog = new Cat(x,y,catimg,1);
-        this.animals.push(dog);
-    }
-    public animate(){
-        for(const animal of this.animals){
-            animal.draw();
-            animal.update();
-        }
-        requestAnimationFrame(()=>this.animate()!);
-    }
+    requestAnimationFrame(() => this.animate()!);
+  }
 }
