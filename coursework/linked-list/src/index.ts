50c38fde898c7a0c03fa70ee3ae6f5d7600dbc48
SeverinGusenleitner <severin.gusenleitner@gmail.com>
2026-04-10 12:40:01 +0200

    added html structure and basic styling

--- coursework/linked-list/src/index.ts
+++ coursework/linked-list/src/index.ts
@@ -1,9 +1,7 @@
 import { LinkedList} from "./linkedList";
+import './styles.css';
 const linkedList = new LinkedList();
-linkedList.addNodeInStart({title:"flashing lights",artist:"kanye"});
-linkedList.addNodeInStart({title:"flashing lights",artist:"kanye"});
-linkedList.addNodeInStart({title:"flashing lights",artist:"kanye"});
-linkedList.addNodeInStart({title:"flashing lights",artist:"kanye"});
-linkedList.insertAfter(2,{title:"billie jean",artist:"micheal jackson"});
-linkedList.addNodeToTail({title:"blinding lights", artist:"the weeknd"})
+linkedList.addNodeInStart({title:"flashing lights",artist:"kanye west"});
+linkedList.insertAfter(1,{title:"billie jean",artist:"micheal jackson"});
+linkedList.addNodeToTail({title:"blinding lights", artist:"the weeknd"});
 linkedList.logNodes();
