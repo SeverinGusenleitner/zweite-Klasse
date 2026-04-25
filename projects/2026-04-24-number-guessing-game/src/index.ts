392ee3d951ccee3038e1adca99b393d14b538287
SeverinGusenleitner <severin.gusenleitner@gmail.com>
2026-04-25 11:19:44 +0200

    added a history

--- projects/2026-04-24-number-guessing-game/src/index.ts
+++ projects/2026-04-24-number-guessing-game/src/index.ts
@@ -4,29 +4,40 @@ class NumberGuessingGame{
     private resetEl:HTMLButtonElement;
     private guessEl:HTMLButtonElement;
     private feedbackEl: HTMLParagraphElement; 
+    private historyEl: HTMLDivElement;
     private randomNum = this.getRandomNum();
+    
     private guess:number = 0;
-    constructor(inputId:string = "input",resetId:string = "reset-btn", guessId:string ="guess-btn", feedbackId:string = "feedback"){
+    private prevGuesses:number[] = [];
+    constructor(inputId:string = "input",resetId:string = "reset-btn", guessId:string ="guess-btn", feedbackId:string = "feedback", historyId:string = "history-div"){
         this.inputEl = document.getElementById(inputId) as HTMLInputElement;
         this.resetEl = document.getElementById(resetId) as HTMLButtonElement;
         this.guessEl = document.getElementById(guessId) as HTMLButtonElement;
-        this.feedbackEl = document.getElementById(feedbackId) as HTMLParagraphElement
+        this.feedbackEl = document.getElementById(feedbackId) as HTMLParagraphElement;
+        this.historyEl = document.getElementById(historyId) as HTMLDivElement;
         this.guessEl.addEventListener("click",()=> {
             this.guess = this.inputEl.valueAsNumber;
             this.giveFeedback();
         })
+        this.renderPrevGuesses();
         this.resetEl.addEventListener("click",()=>this.reset());
         this.inputEl.value = "0";
     }
     private giveFeedback(){
+        this.prevGuesses.push(this.guess);
         if(this.guess  === this.randomNum){
             this.feedbackEl.textContent = "you guessed correctly!";
-        }else if(this.guess < this.randomNum){
-            this.feedbackEl.textContent = "Higher!";
         }else{
-            this.feedbackEl.textContent = "Lower!";
+            if(this.guess < this.randomNum){
+                this.feedbackEl.textContent = "Higher!";
+            }else{
+                this.feedbackEl.textContent = "Lower!";
+            }
         }
+        this.renderPrevGuesses();
     }
+
+    
     private getRandomNum():number{
         return Math.floor(Math.random()*99)+1;
     }
@@ -35,6 +46,16 @@ class NumberGuessingGame{
         this.randomNum = this.getRandomNum();
         this.feedbackEl.innerHTML = "";
         this.inputEl.value = "0";
+        this.prevGuesses = [];
+        this.renderPrevGuesses();
+    }
+    private renderPrevGuesses(){
+        this.historyEl.innerHTML = "";
+        for(const guess of this.prevGuesses){
+            const guessEl = document.createElement("div");
+            guessEl.textContent = `${guess}`
+            this.historyEl.appendChild(guessEl);
+        }
     }
 }
 const game = new NumberGuessingGame();
