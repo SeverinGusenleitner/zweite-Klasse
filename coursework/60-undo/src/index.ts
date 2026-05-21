a634e52cf592c7f093c8ad32393503db55943dac
SeverinGusenleitner <severin.gusenleitner@gmail.com>
2026-05-21 15:18:36 +0200

    added redo

--- coursework/60-undo/src/index.ts
+++ coursework/60-undo/src/index.ts
@@ -2,40 +2,52 @@ import { UndoStack,AddCommand,SubCommand,Command,ResetCommand } from "./linkedLi
 import './styles.css';
 const addBtn = document.getElementById("addBtn") as HTMLButtonElement;
 const subBtn = document.getElementById("subBtn") as HTMLButtonElement;
-const inputEl = document.getElementById("inputEl") as HTMLInputElement;
 const undoBtn = document.getElementById("undoBtn") as HTMLButtonElement;
 const resetBtn = document.getElementById("resetBtn") as HTMLButtonElement;
+const redoBtn = document.getElementById("redoBtn") as HTMLButtonElement;
+const inputEl = document.getElementById("inputEl") as HTMLInputElement;
 const currentValueEl = document.getElementById("currentValueEl") as HTMLSpanElement;
 
-const stack = new UndoStack<Command>();
+const undoStack = new UndoStack<Command>();
+const redoStack = new UndoStack<Command>();
+
 let currentValue = 0;
 inputEl.value = `0`;
 
 addBtn.addEventListener("click", ()=>{
   const command = new AddCommand(inputEl.valueAsNumber);
-  stack.push(command)
+  undoStack.push(command)
   currentValue = command.execute(currentValue);
   currentValueEl.textContent = `${currentValue}`;
 })
 
 subBtn.addEventListener("click", ()=>{
     const command = new SubCommand(inputEl.valueAsNumber);
-    stack.push(command)
+    undoStack.push(command)
     currentValue = command.execute(currentValue);
     currentValueEl.textContent = `${currentValue}`;
 })
 
 undoBtn.addEventListener("click", ()=>{
-  const command = stack.pop()
+  const command = undoStack.pop()
   if(command){
     currentValue = command.undo(currentValue);
     currentValueEl.textContent = `${currentValue}`
+    redoStack.push(command);
   }
 })
 
 resetBtn.addEventListener("click", ()=>{
   const command = new ResetCommand();
-  stack.push(command);
+  undoStack.push(command);
   currentValue = command.execute(currentValue);
   currentValueEl.textContent = `${currentValue}`;
+})
+
+redoBtn.addEventListener("click", ()=>{
+  const command = redoStack.pop()
+  if(command){
+    currentValue = command.execute(currentValue);
+    currentValueEl.textContent = `${currentValue}`
+  }
 })
