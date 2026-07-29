01f09c01db980c81946253b9bb83e823a6e5e53a
SeverinGusenleitner <severin.gusenleitner@gmail.com>
2026-07-29 16:19:28 +0200

    added method to move the Bar

--- projects/2026-07-29-bar-sorter/src/Bar.ts
+++ projects/2026-07-29-bar-sorter/src/Bar.ts
@@ -16,4 +16,15 @@ class Bar {
     this.barElement.setAttribute('height', `${this.height}`);
     this.svgElement.appendChild(this.barElement);
   }
+  public moveBar(x:number = this.x, y:number = this.y, width:number = this.width, height:number = this.height):void{
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.barElement.setAttribute('x', `${this.x}`);
+    this.barElement.setAttribute('y', `${this.y}`);
+    this.barElement.setAttribute('width', `${this.width}`);
+    this.barElement.setAttribute('height', `${this.height}`);
+  }
 }
+export {Bar};
