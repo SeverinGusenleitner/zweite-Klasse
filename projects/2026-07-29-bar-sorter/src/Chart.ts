136c4d2ca917dd986dc530358ace5c81660bd3b6
SeverinGusenleitner <severin.gusenleitner@gmail.com>
2026-07-29 17:14:52 +0200

    removed randomize button, as it basicly does the exact same thing as set

--- projects/2026-07-29-bar-sorter/src/Chart.ts
+++ projects/2026-07-29-bar-sorter/src/Chart.ts
@@ -1,55 +1,54 @@
-import { bubbleSort } from "./SortMethod";
-import { Bar } from "./Bar";
-class Chart{
-    private readonly svgElement: SVGSVGElement;
-    private readonly WIDTH;
-    private readonly HEIGHT;
-    private bars:number[] = [];
-    private amountOfBars : number = 50;
-    private bubbleSort = new bubbleSort();
-    constructor(svgId:string = "canvas"){
-        this.svgElement = document.getElementById(svgId) as unknown as SVGSVGElement;
-        this.WIDTH = this.svgElement.clientWidth;
-        this.HEIGHT = this.svgElement.clientHeight;
-        this.randomize();
-        this.draw();
+import { bubbleSort } from './SortMethod';
+import { Bar } from './Bar';
+class Chart {
+  private readonly svgElement: SVGSVGElement;
+  private readonly WIDTH;
+  private readonly HEIGHT;
+  private bars: number[] = [];
+  private amountOfBars: number = 50;
+  private bubbleSort = new bubbleSort();
+  constructor(svgId: string = 'canvas') {
+    this.svgElement = document.getElementById(svgId) as unknown as SVGSVGElement;
+    this.WIDTH = this.svgElement.clientWidth;
+    this.HEIGHT = this.svgElement.clientHeight;
+    this.randomize();
+    this.draw();
+  }
+  private randomize() {
+    this.bars = [];
+    while (this.bars.length !== this.amountOfBars) {
+      let randomNum;
+      do {
+        randomNum = Math.floor(Math.random() * this.HEIGHT);
+      } while (this.isAlreadyUsed(randomNum));
+      this.bars.push(randomNum);
     }
-    public randomize(){
-        this.bars = [];
-        while(this.bars.length !== this.amountOfBars){
-            let randomNum;
-            do{
-                randomNum = Math.floor(Math.random()*this.HEIGHT);
-            }while(this.isAlreadyUsed(randomNum))
-            this.bars.push (randomNum);
-        }
+  }
+  public sort(sortMethod: string) {
+    this.bubbleSort.sort(this.bars);
+    console.log(this.bars);
+    this.draw();
+  }
+  private draw() {
+    this.svgElement.innerHTML = '';
+    const barWidth = this.WIDTH / this.amountOfBars;
+    for (let i = 0; i < this.bars.length; i++) {
+      new Bar(i * barWidth, 400 - this.bars[i]!, barWidth, this.bars[i]!);
+    }
+  }
+  public set(amountOfBars: number) {
+    this.amountOfBars = amountOfBars;
+    this.randomize();
+    this.draw();
+  }
 
+  private isAlreadyUsed(height: number) {
+    for (let i = 0; i < this.bars.length; i++) {
+      if (this.bars[i] === height) {
+        return true;
+      }
     }
-    public sort(sortMethod:string){
-        this.bubbleSort.sort(this.bars);
-        console.log(this.bars);
-        this.draw();
-    }
-    private draw(){
-        this.svgElement.innerHTML = "";
-        const barWidth = this.WIDTH/this.amountOfBars;
-        for(let i = 0; i<this.bars.length;i++){
-            new Bar(i*barWidth,400-this.bars[i]!,barWidth,this.bars[i]!)
-        }
-    }
-    public set(amountOfBars:number){
-        this.amountOfBars = amountOfBars;
-        this.randomize();
-        this.draw();
-    }
-
-    private isAlreadyUsed(height:number){
-        for(let i = 0; i<this.bars.length; i++){
-            if(this.bars[i] === height){
-                return true;
-            }
-        }
-        return false;
-    }
+    return false;
+  }
 }
-export{Chart};
+export { Chart };
--- projects/2026-07-29-bar-sorter/src/index.ts
+++ projects/2026-07-29-bar-sorter/src/index.ts
@@ -3,7 +3,6 @@ import { Chart } from './Chart';
 const chart = new Chart();
 const setBtn = document.getElementById("set-btn") as HTMLButtonElement;
 const sortBtn = document.getElementById("sort-btn") as HTMLButtonElement;
-const randomizeBtn = document.getElementById("randomize-btn") as HTMLButtonElement;
 
 const slider = document.getElementById("slider") as HTMLInputElement;
 setBtn.addEventListener("click", ()=>{
@@ -11,7 +10,4 @@ setBtn.addEventListener("click", ()=>{
 })
 sortBtn.addEventListener("click" , ()=>{
     chart.sort("bubble-sort");
-})
-randomizeBtn.addEventListener("click", ()=>{
-    chart.randomize();
 })
